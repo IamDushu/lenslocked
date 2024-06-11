320e6379fb0db310fda1c39382299dfa789634e0
IamDushu <hey.dushyanth@gmail.com>
2024-06-12 00:35:32 +0530

    Exercises - templates

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -2,29 +2,106 @@ package main
 
 import (
 	"html/template"
-	"os"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
+//Exercises
+// Ex.1 Add new fields,
+// Ex.2 Experiment diff data types
+// Ex.3 Add nested data and access
+// Ex.4 Try to add if/else inside template
+
 type User struct {
-	Name string
-	Bio string
-	Age int
+	Name       string
+	Bio        string
+	Age        int
+	Experience float64
+	Education  map[string]string
+	Likes      []string
+	Friends    []User
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
 
-	user := User{
-		Name: "Dhana",
-		Bio: "<script>alert(`You're hacked`)</script>",
-		Age: 122,
-	}
+	r := chi.NewRouter()
 
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
+	r.Get("/template", func(w http.ResponseWriter, r *http.Request) {
+
+		t, err := template.ParseFiles("hello.gohtml")
+		if err != nil {
+			panic(err)
+		}
+
+		myLikes := []string{}
+		myLikes = append(myLikes, "PaniPuri", "Perugu")
+
+		user1 := User{
+			Name: "Chaithanya",
+		}
+
+		user2 := User{
+			Name: "Dushyanth",
+		}
+
+		user := User{
+			Name:       "Dhana",
+			Bio:        "<script>alert(`You're hacked`)</script>",
+			Age:        122,
+			Experience: 1.2,
+			Education: map[string]string{
+				"10":     "Vidyanikethan",
+				"12":     "Chaithanya-Kazana",
+				"Degree": "SVU",
+			},
+			Likes:   myLikes[:1],
+			Friends: []User{user1, user2},
+		}
+
+		err = t.Execute(w, user)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	http.HandleFunc("/template", func(w http.ResponseWriter, r *http.Request) {
+
+		t, err := template.ParseFiles("hello.gohtml")
+		if err != nil {
+			panic(err)
+		}
+
+		myLikes := []string{}
+		myLikes = append(myLikes, "PaniPuri", "Perugu")
+
+		user1 := User{
+			Name: "Chaithanya",
+		}
+
+		user2 := User{
+			Name: "Dushyanth",
+		}
+
+		user := User{
+			// Name:       "Dhana",
+			Bio:        "<script>alert(`You're hacked`)</script>",
+			Age:        122,
+			Experience: 1.2,
+			Education: map[string]string{
+				"10":     "Vidyanikethan",
+				"12":     "Chaithanya-Kazana",
+				"Degree": "SVU",
+			},
+			Likes:   myLikes[:1],
+			Friends: []User{user1, user2},
+		}
+
+		err = t.Execute(w, user)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	http.ListenAndServe(":3000", nil) //instead of nil if you use 'r' then chi get's used
 }
