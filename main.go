31af3bfbdd8ce2ccb530ef9c17cfbde34ea3fafd
IamDushu <hey.dushyanth@gmail.com>
2024-06-11 21:38:05 +0530

    Added an executeTemplate function to render templates, and updated the contact page to use a template

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filePath string) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+
+	tpl, err := template.ParseFiles(filePath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
@@ -28,15 +28,20 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:hey.dushyanth@gmail.com\">hey.dushyanth@gmail.com</a></p>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(
-		w, 
+		w,
 		`
 		<h1>FAQ Page</h1>
 		<h2>Q: Is there a free version?</h2>
@@ -49,14 +54,14 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
-func paramHandler (w http.ResponseWriter, r *http.Request) {
+func paramHandler(w http.ResponseWriter, r *http.Request) {
 	param := chi.URLParam(r, "id")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `<h1>Your Id is %s</h1>`, param)
 }
 
 func main() {
-	
+
 	r := chi.NewRouter()
 
 	r.Get("/", homeHandler)
@@ -70,4 +75,3 @@ func main() {
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
-  
