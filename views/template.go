1a8c86e46d001d1e5b5060c5d45d78de79e7ab72
IamDushu <hey.dushyanth@gmail.com>
2024-06-12 21:25:20 +0530

    Made the FAQ page use dynamically provided questions and answers

--- views/template.go
+++ views/template.go
@@ -38,7 +38,7 @@ type Template struct {
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
 
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
