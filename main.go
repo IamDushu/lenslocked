d588c77ad3599c06d41a5cc83eef0ad863047cb3
IamDushu <hey.dushyanth@gmail.com>
2024-06-15 20:31:35 +0530

    Added Sign In view

--- main.go
+++ main.go
@@ -37,7 +37,9 @@ func main() {
 		UserService: &userService, 
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
 
+	r.Get("/signin", usersC.SignIn)
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 
