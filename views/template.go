5c5e26c00dd9f6f545ba3a52e7d1d9a2c0a2d9cb
IamDushu <hey.dushyanth@gmail.com>
2024-06-16 15:06:08 +0530

    Exploring template function errors

--- views/template.go
+++ views/template.go
@@ -1,13 +1,13 @@
 package views
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/csrf"
 )
 
 func Must(t Template, err error) Template {
@@ -21,8 +21,8 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	tpl := template.New(patterns[0])
 	tpl = tpl.Funcs(
 		template.FuncMap{
-			"csrfField": func() template.HTML {
-				return `<!-- TODO: Implement the csrfField -->`
+			"csrfField": func() (template.HTML, error) {
+				return "", fmt.Errorf("csrfField not implemented")
 			},
 		},
 	)
@@ -54,17 +54,20 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 	}
 	tpl = tpl.Funcs(
 		template.FuncMap{
-			"csrfField": func() template.HTML {
-				return csrf.TemplateField(r)
-			},
+			// "csrfField": func() template.HTML {
+			// 	return csrf.TemplateField(r)
+			// },
 		},
 	)
 
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-	err = tpl.Execute(w, data)
+	var buff bytes.Buffer
+	err = tpl.Execute(&buff, data)
+
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
 		return
 	}
+	io.Copy(w, &buff)
 }
