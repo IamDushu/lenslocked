e6a41a102880bf90413e5ea62af264f7bbc44933
IamDushu <hey.dushyanth@gmail.com>
2024-06-18 20:06:43 +0530

    Accessing the current user in templates

--- views/template.go
+++ views/template.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/csrf"
+	"github.com/iamDushu/lenslocked/context"
+	"github.com/iamDushu/lenslocked/models"
 )
 
 func Must(t Template, err error) Template {
@@ -25,6 +27,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		template.FuncMap{
 			"csrfField": func() (template.HTML, error) {
 				return "", fmt.Errorf("csrfField not implemented")
+			},
+			"currentUser": func() (template.HTML, error) {
+				return "", fmt.Errorf("currentUser not implemented")
 			},
 		},
 	)
@@ -58,6 +63,10 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		template.FuncMap{
 			"csrfField": func() template.HTML {
 				return csrf.TemplateField(r)
+			},
+			"currentUser": func() *models.User {
+				ctx := r.Context()
+				return context.User(ctx)
 			},
 		},
 	)
