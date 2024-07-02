cf2e0482e900ada71ca4d2b87ecd5a5588ac7172
IamDushu <hey.dushyanth@gmail.com>
2024-07-02 16:01:41 +0530

    Update gallery handler

--- main.go
+++ main.go
@@ -150,6 +150,7 @@ func main() {
 			r.Get("/new", galleriesC.New)
 			r.Post("/", galleriesC.Create)
 			r.Get("/{id}/edit", galleriesC.Edit)
+			r.Post("/{id}", galleriesC.Update)
 		})
 	})
 	// r.Get("/users/me", usersC.CurrentUser)
