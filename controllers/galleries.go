2cee1467db7d64b9b391d3f2c678147a88cbcac7
IamDushu <hey.dushyanth@gmail.com>
2024-07-14 18:11:09 +0530

    URL Path Escaping Image Filenames

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
@@ -52,8 +53,9 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	type Image struct {
-		GalleryID int
-		Filename  string
+		GalleryID       int
+		Filename        string
+		FilenameEscaped string
 	}
 	var data struct {
 		ID     int
@@ -69,8 +71,9 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 	}
 	for _, image := range images {
 		data.Images = append(data.Images, Image{
-			GalleryID: image.GalleryID,
-			Filename:  image.Filename,
+			GalleryID:       image.GalleryID,
+			Filename:        image.Filename,
+			FilenameEscaped: url.PathEscape(image.Filename),
 		})
 	}
 	g.Templates.Show.Execute(w, r, data)
