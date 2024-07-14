77eec866a74c8b8933c8efeb961555cba2460304
IamDushu <hey.dushyanth@gmail.com>
2024-07-14 18:41:06 +0530

    Delete Image Service Func

--- models/gallery.go
+++ models/gallery.go
@@ -146,6 +146,18 @@ func (service *GalleryService) Image(galleryId int, filename string) (Image, err
 	}, nil
 }
 
+func (service *GalleryService) DeleteImage(galleryId int, filename string) error {
+	image, err := service.Image(galleryId, filename)
+	if err != nil {
+		return fmt.Errorf("deleting image: %w", err)
+	}
+	err = os.Remove(image.Path)
+	if err != nil {
+		return fmt.Errorf("deleting image: %w", err)
+	}
+	return nil
+}
+
 func (service *GalleryService) extensions() []string {
 	return []string{".png", ".jpg", ".jpeg", ".gif"}
 }
