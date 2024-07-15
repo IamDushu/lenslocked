0d1621483cd330837f0033ac7eef07baabc411d7
IamDushu <hey.dushyanth@gmail.com>
2024-07-15 12:53:23 +0530

    Rendering Content Type Errors

--- models/gallery.go
+++ models/gallery.go
@@ -147,9 +147,17 @@ func (service *GalleryService) Image(galleryId int, filename string) (Image, err
 	}, nil
 }
 
-func (service *GalleryService) CreateImage(galleryID int, filename string, contents io.Reader) error {
+func (service *GalleryService) CreateImage(galleryID int, filename string, contents io.ReadSeeker) error {
+	err := checkContentType(contents, service.imageContentTypes())
+	if err != nil {
+		return fmt.Errorf("creating image %v: %w", filename, err)
+	}
+	err = checkExtension(filename, service.extensions())
+	if err != nil {
+		return fmt.Errorf("creating image %v: %w", filename, err)
+	}
 	galleryDir := service.galleryDir(galleryID)
-	err := os.MkdirAll(galleryDir, 0755)
+	err = os.MkdirAll(galleryDir, 0755)
 	if err != nil {
 		return fmt.Errorf("creating gallery-%d images directory: %w", galleryID, err)
 	}
@@ -180,6 +188,10 @@ func (service *GalleryService) DeleteImage(galleryId int, filename string) error
 
 func (service *GalleryService) extensions() []string {
 	return []string{".png", ".jpg", ".jpeg", ".gif"}
+}
+
+func (service *GalleryService) imageContentTypes() []string {
+	return []string{"image/jpeg", "image/jpg", "image/png", "image/gif"}
 }
 
 func (service *GalleryService) galleryDir(id int) string {
